14823ce17b90604c91cc6f6026c38ac032a63980
Marcello Alves <marcello_doalves@hotmail.com>
2021-12-29 15:47:37 -0300

    Adding logic to roll the dices.

--- IOS-RollDice/ViewController.swift
+++ IOS-RollDice/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var dice_01: UIImageView!
+    
+    @IBOutlet weak var dice_02: UIImageView!
+    
+    let arrImgs: Array = [UIImage.init(imageLiteralResourceName: "DiceOne"),
+                          UIImage.init(imageLiteralResourceName: "DiceTwo"),
+                          UIImage.init(imageLiteralResourceName: "DiceThree"),
+                          UIImage.init(imageLiteralResourceName: "DiceFour"),
+                          UIImage.init(imageLiteralResourceName: "DiceFive"),
+                          UIImage.init(imageLiteralResourceName: "DiceSix")];
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollDice(_ sender: Any) {
+        
+        dice_01.image = arrImgs[randomNumber()];
+        
+        dice_02.image = arrImgs[randomNumber()];
+    }
+    
+    func randomNumber() -> Int {
+        
+        return Int.random(in: 0..<5);
+    }
 }
 
